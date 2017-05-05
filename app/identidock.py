2d9bd3fcd819742fa1c5c901ca553fb2007c3a39
tmclean <tjrage19@gmail.com>
2017-05-05 02:13:56 +0000

    Big bang

---
+++ app/identidock.py
@@ -0,0 +1,53 @@
+from flask import Flask, Response, request
+import requests
+import hashlib
+import redis
+
+app = Flask( __name__ )
+cache = redis.StrictRedis(host='redis', port=6379, db=0)
+salt = "UNIQUE_SALT1701"
+default_name = 'Joe Bloggs'
+
+@app.route('/', methods=['GET', 'POST'])
+def mainpage():
+
+   name = default_name
+   if request.method == 'POST':
+      name = request.form['name']
+
+   salted_name = salt + name
+   name_hash = hashlib.sha256( salted_name.encode() ).hexdigest()
+
+   html = '''
+      <html>
+         <head>
+            <title>Identidock</title>
+         <head>
+         <body>
+            <form method="POST">
+               Hello <input type="text" name="name" value="{0}">
+               <input type="submit" value="submit">
+            </form>
+            <p>You look like a:
+            <img src="/monster/{1}"/>
+         </body>
+      </html>
+      '''.format( name, name_hash )     
+
+   return html
+
+@app.route('/monster/<name>')
+def get_identicon(name):
+
+   image = cache.get( name )
+   if image is None:
+      print( "Cache miss", flush=True )
+      r = requests.get('http://dnmonster:8080/monster/' + name + '?size=80' )
+      image = r.content
+      cache.set(name, image)
+   
+   return Response( image, mimetype='image/png' )
+
+if __name__ == '__main__':
+   app.run( debug=True, host='0.0.0.0' )
+
